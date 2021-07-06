7c81729d92f2ed3260cb809754e9e93b5854f0d6
repolevedavaj <jonas.bamberger@gmail.com>
2021-07-06 22:25:53 +0200

    upgraded version of CLI

--- Casks/project-env-cli.rb
+++ Casks/project-env-cli.rb
@@ -1,6 +1,6 @@
 cask "project-env-cli" do
-  version "3.0.0"
-  sha256 "5421a3a0a3e440ff43ddc98795eada91e7fdba8971d13809284766c1d4bc513a"
+  version "3.0.1"
+  sha256 "54383cd799eb221d6b44a729f1d0531877569415841ad7a759ad40a9c894ec45"
 
   url "https://github.com/Project-Env/project-env-cli/releases/download/v#{version}/cli-#{version}-macos-amd64.tar.gz"
   name "project-env-cli"
