a75dbb08c9fa187dd6631f2524e613ae5a84d68a
repolevedavaj <jonas.bamberger@gmail.com>
2021-10-08 21:45:12 +0200

    upgraded project-env-cli to 3.2.1

--- Casks/project-env-cli.rb
+++ Casks/project-env-cli.rb
@@ -1,6 +1,6 @@
 cask "project-env-cli" do
-  version "3.2.0"
-  sha256 "5e433a557050ddeffaeec2fa6551844a33a96fe4e38590eee496e629895982e0"
+  version "3.2.1"
+  sha256 "39d9f429b686e7671fc81f851e02ac2bc6d24dab769cf496a21d5f6cf171bb58"
 
   url "https://github.com/Project-Env/project-env-cli/releases/download/v#{version}/cli-#{version}-macos-amd64.tar.gz"
   name "project-env-cli"
--- etc/scripts/create-cli-sha256-checksum.sh
+++ etc/scripts/create-cli-sha256-checksum.sh
@@ -1,4 +1,4 @@
-VERSION="3.2.0"
+VERSION="3.2.1"
 URL="https://github.com/Project-Env/project-env-cli/releases/download/v${VERSION}/cli-${VERSION}-macos-amd64.tar.gz"
 
 echo "creating checksum for $URL"
