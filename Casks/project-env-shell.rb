313835c5197676f5ba678e6ce194812ae8f783db
repolevedavaj <jonas.bamberger@gmail.com>
2023-04-06 12:29:34 +0200

    bump project-env-shell to 3.2.0

--- Casks/project-env-shell.rb
+++ Casks/project-env-shell.rb
@@ -1,8 +1,8 @@
 cask "project-env-shell" do
-  version "3.1.0"
-  sha256 "29456ab381231a2921c41b9b223eeec058bba06d1d46974e5594ad9afb101d2d"
+  version "3.2.0"
+  sha256 "505836e432e0da41c6865f6aae4118b2346624273307b5b9287727a1bd484296"
 
-  url "https://github.com/Project-Env/project-env-shell/releases/download/v#{version}/shell-#{version}-macos-amd64.tar.gz"
+  url "https://github.com/Project-Env/project-env-shell/releases/download/v3.2.0/shell-3.2.0-macos-amd64.tar.gz"
 
   name "project-env-cli"
   desc "Project-Env shell integration"
