053617453366c05990251d3443f2db1d28e2ff0f
Jonas Scheiwiller <jonas.scheiwiller@sumex.ch>
2024-10-01 17:04:08 +0200

    Switch to aarch64 release

--- Casks/project-env-cli.rb
+++ Casks/project-env-cli.rb
@@ -1,8 +1,8 @@
 cask "project-env-cli" do
   version "3.19.0"
-  sha256 "4e8f6934146c4074b07e7391aad70803201750e3f0f96d1638f3680f8f92a0ae"
+  sha256 "a44844e83aa6141bb614ed5eee7dd1546e44101f31bada644575a4bb4bee99d3"
 
-  url "https://github.com/Project-Env/project-env-cli/releases/download/v3.19.0/cli-3.19.0-macos-amd64.tar.gz"
+  url "https://github.com/Project-Env/project-env-cli/releases/download/v3.19.0/cli-3.19.0-macos-aarch64.tar.gz"
   name "project-env-cli"
   desc "Project-Env CLI"
   homepage "https://project-env.github.io"
