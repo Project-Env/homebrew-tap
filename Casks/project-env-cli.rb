10cce46829dfebd83b4d8324d3cbfd312e6a73fd
repolevedavaj <jonas.bamberger@gmail.com>
2025-06-22 13:21:27 +0200

    bump project-env-cli to 3.22.3

--- Casks/project-env-cli.rb
+++ Casks/project-env-cli.rb
@@ -1,8 +1,8 @@
 cask "project-env-cli" do
-  version "3.22.2"
-  sha256 "cbfcc81610a9f9f9e1dd88a954320d786697caf7979565ec3e4b6915634d20c5"
+  version "3.22.3"
+  sha256 "fcba2fd721d67f42dc6ee03b117acc3cb9c649c925c3a874ad6d0518bede29bf"
 
-  url "https://github.com/Project-Env/project-env-cli/releases/download/v3.22.2/cli-3.22.2-macos-aarch64.tar.gz"
+  url "https://github.com/Project-Env/project-env-cli/releases/download/v3.22.3/cli-3.22.3-macos-aarch64.tar.gz"
   name "project-env-cli"
   desc "Project-Env CLI"
   homepage "https://project-env.github.io"
