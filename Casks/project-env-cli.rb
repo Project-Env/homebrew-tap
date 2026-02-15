b6bd37e65f344b1d7fb82b4f8023a9deea83103c
repolevedavaj <jonas.bamberger@gmail.com>
2026-02-15 13:28:19 +0100

    Fix download URLs to include v prefix in release tag path

--- Casks/project-env-cli.rb
+++ Casks/project-env-cli.rb
@@ -5,7 +5,7 @@ cask "project-env-cli" do
   sha256 arm: "6f3441513e3560bb26ef27b06d8fb42e1c3df16e97fbdf42d04db0c9deeb5d11",
          intel: "be0feca78986dc909b4dc1250f9b051bc6c9102f61ff9ea5d307be7d617ab1bc"
 
-  url "https://github.com/Project-Env/project-env-cli/releases/download/#{version}/cli-#{version}-macos-#{arch}.tar.gz"
+  url "https://github.com/Project-Env/project-env-cli/releases/download/v#{version}/cli-#{version}-macos-#{arch}.tar.gz"
   name "project-env-cli"
   desc "Project-Env CLI"
   homepage "https://project-env.github.io/"
--- Formula/project-env-cli.rb
+++ Formula/project-env-cli.rb
@@ -6,22 +6,22 @@ class ProjectEnvCli < Formula
 
   on_macos do
     on_arm do
-      url "https://github.com/Project-Env/project-env-cli/releases/download/#{version}/cli-#{version}-macos-aarch64.tar.gz"
+      url "https://github.com/Project-Env/project-env-cli/releases/download/v#{version}/cli-#{version}-macos-aarch64.tar.gz"
       sha256 "6f3441513e3560bb26ef27b06d8fb42e1c3df16e97fbdf42d04db0c9deeb5d11"
     end
     on_intel do
-      url "https://github.com/Project-Env/project-env-cli/releases/download/#{version}/cli-#{version}-macos-amd64.tar.gz"
+      url "https://github.com/Project-Env/project-env-cli/releases/download/v#{version}/cli-#{version}-macos-amd64.tar.gz"
       sha256 "be0feca78986dc909b4dc1250f9b051bc6c9102f61ff9ea5d307be7d617ab1bc"
     end
   end
 
   on_linux do
     on_arm do
-      url "https://github.com/Project-Env/project-env-cli/releases/download/#{version}/cli-#{version}-linux-aarch64.tar.gz"
+      url "https://github.com/Project-Env/project-env-cli/releases/download/v#{version}/cli-#{version}-linux-aarch64.tar.gz"
       sha256 "919db20d1b6c2cad99ba1a3917b613b436b46fd750e9eb300cdc6ed7981dc670"
     end
     on_intel do
-      url "https://github.com/Project-Env/project-env-cli/releases/download/#{version}/cli-#{version}-linux-amd64.tar.gz"
+      url "https://github.com/Project-Env/project-env-cli/releases/download/v#{version}/cli-#{version}-linux-amd64.tar.gz"
       sha256 "2018ae893b7bf81c83bfc2fbe6fce0228d0bad847e6c1a3761eb4f1f7c9659fe"
     end
   end
